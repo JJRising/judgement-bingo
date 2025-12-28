3229b8836dbd8c4c6ac51f637ce336b53add91bd
James Grubb <james2grubb@gmail.com>
2025-12-27 22:35:04 -0500

    bootstrap local development of the spring boot app. Will add a react project to mount in the container and serve from there. Will likely mount a volume for it to get hot reload for that.

---
+++ backend/src/main/java/com/jjrising/bingo/JudgementBingoApplication.java
@@ -0,0 +1,13 @@
+package com.jjrising.bingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgementBingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudgementBingoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jjrising/bingo/JudgementBingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjrising.bingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgementBingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

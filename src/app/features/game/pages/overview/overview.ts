e65e9c78cc122bf0b2eba609be2e5f5f8df0fc2e
jjrising <james2grubb@gmail.com>
2026-05-23 14:06:39 -0400

    Setup components for the subpages within a game.

---
+++ src/app/features/game/pages/overview/overview.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './overview.html',
+  styleUrl: './overview.css',
+})
+export class Admin {}
---
+++ src/app/features/game/pages/prompts/prompts.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './prompts.html',
+  styleUrl: './prompts.css',
+})
+export class Admin {}

c3d4b937b013da12019222e1ca0edc0c139e9684
jjrising <james2grubb@gmail.com>
2026-05-24 12:15:33 -0400

    Fixing width and animation of sidenav/main content

--- src/app/features/game/game.ts
+++ src/app/features/game/game.ts
@@ -1,8 +1,8 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, inject, signal, viewChild} from '@angular/core';
 import {ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {map} from 'rxjs';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSidenavContainer, MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -15,7 +15,14 @@ import {MatButtonModule} from '@angular/material/button';
 })
 export class Game {
     private readonly route = inject(ActivatedRoute);
+    private readonly container = viewChild.required(MatSidenavContainer);
 
     readonly gameId = toSignal(this.route.paramMap.pipe(map(params => params.get('id'))));
     readonly collapsed = signal(false);
+
+    toggleCollapsed(): void {
+        this.collapsed.update(v => !v);
+        // Timeout so recalculation of main content width happens after the sidenav animation
+        setTimeout(() => this.container().updateContentMargins(), 210);
+    }
 }
